a56b8a3405254d1b609ba60770f73e6cbfa47cc3
sandeepraj96 <95302937+sandeepraj96@users.noreply.github.com>
2023-08-19 11:44:51 +0530

    newjenfiles

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -35,6 +35,14 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
+  
+  @Test
+  public void greetShouldIncludeGreetingPhrase() {
+    String someone = "sandeep";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
